503a2605094564400542a0fc52910c180c1d70c6
Amal <amal.kallel94@gmail.com>
2018-03-02 01:45:47 +0100

    commit

--- src/main/java/com/cartus/entities/Administrateur.java
+++ src/main/java/com/cartus/entities/Administrateur.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-//2em commit
+
 @Entity
 public class Administrateur implements Serializable{
 	
