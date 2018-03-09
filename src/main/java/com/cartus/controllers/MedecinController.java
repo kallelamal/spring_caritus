0776de07425cd7ecf603837c8ad6e75b7fae8a01
mrw007 <mr.wahib@gmail.com>
2018-03-10 00:03:04 +0100

    json update

--- src/main/java/com/cartus/controllers/MedecinController.java
+++ src/main/java/com/cartus/controllers/MedecinController.java
@@ -23,7 +23,6 @@ public class MedecinController {
 	public Medecin signupMedecin(Medecin m) {
 		return medecinMetier.signupMedecin(m);
 	}*/
-	
 	@RequestMapping(value="/signupMedecin",method=RequestMethod.POST)
 	public Medecin signupMedecin(@RequestBody String m) throws Exception {
 			
