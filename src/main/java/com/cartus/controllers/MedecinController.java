c0da0c21fe9b75f618fe9b1fe9cf1c7a7db4e755
mrw007 <mr.wahib@gmail.com>
2018-03-09 23:58:03 +0100

    json update

--- src/main/java/com/cartus/controllers/MedecinController.java
+++ src/main/java/com/cartus/controllers/MedecinController.java
@@ -1,24 +1,37 @@
 package com.cartus.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cartus.entities.Medecin;
 import com.cartus.metier.MedecinMetier;
+import com.google.gson.Gson;
 
 @RestController
 public class MedecinController {
+	Gson json =new Gson();
 	@Autowired
 	private  MedecinMetier medecinMetier;
 
-	@RequestMapping(value="/signupMedecin",method=RequestMethod.POST)
+	/*@RequestMapping(value="/signupMedecin",method=RequestMethod.POST)
 	public Medecin signupMedecin(Medecin m) {
 		return medecinMetier.signupMedecin(m);
-	}
+	}*/
+	
+	@RequestMapping(value="/signupMedecin",method=RequestMethod.POST)
+	public Medecin signupMedecin(@RequestBody String m) throws Exception {
+			
+		Medecin med=json.fromJson(m,Medecin.class);
+		return medecinMetier.signupMedecin(med);
+
+		}
+
 
 	@RequestMapping(value="/signinMedecin",method=RequestMethod.GET)
 	public Medecin signinMedecin(String login, String password) {
