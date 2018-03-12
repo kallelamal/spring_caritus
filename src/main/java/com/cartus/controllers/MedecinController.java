05dd8de503992222d80d92798a96968b248bcd58
mrw007 <mr.wahib@gmail.com>
2018-03-12 17:32:39 +0100

    Working return medecin

--- src/main/java/com/cartus/controllers/MedecinController.java
+++ src/main/java/com/cartus/controllers/MedecinController.java
@@ -1,11 +1,16 @@
 package com.cartus.controllers;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cartus.entities.Medecin;
 import com.cartus.metier.MedecinMetier;
@@ -14,10 +19,17 @@ import com.cartus.metier.MedecinMetier;
 public class MedecinController {
 	@Autowired
 	private  MedecinMetier medecinMetier;
-
+	
 	@RequestMapping(value="/signupMedecin",method=RequestMethod.POST)
-	public Medecin signupMedecin(@RequestBody Medecin m) {
-		return medecinMetier.signupMedecin(m);
+	public ResponseEntity<Medecin> signupMedecin( @RequestBody Medecin m) {
+
+		Medecin med = medecinMetier.signupMedecin(m);
+
+		if (med == null)
+			 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+		return new ResponseEntity<>(medecinMetier.signupMedecin(m), HttpStatus.CREATED);
+		
 	}
 
 	@RequestMapping(value="/signinMedecin",method=RequestMethod.GET)
@@ -34,4 +46,9 @@ public class MedecinController {
 	public List<Medecin> getMedecinByValidation(int validation) {
 		return medecinMetier.getMedecinByValidation(validation);
 	}
+	
+	@RequestMapping(value="/getMedecinById",method=RequestMethod.GET)
+	public Medecin getMedecinById(Long id) {
+		return medecinMetier.getMedecinById(id);
+				}
 }
